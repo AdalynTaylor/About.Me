534966eabf9ce50a12b93bba42a7a69d49e3fee3
Adalynity <adalynity@gmail.com>
2023-09-25 19:33:23 +0100

    added functional print button to CV

--- JavaScript Files/CVScript.js
+++ JavaScript Files/CVScript.js
@@ -1,8 +1,5 @@
-window.addEventListener("scroll", function(){
-  var header = document.querySelector("navBar");
-  header.classList.toggle("sticky", window.scrollY > 0)
-})
+
 
 function printPage(){
-  window.print;
+  window.print();
 }
